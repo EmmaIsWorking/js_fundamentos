cb2a57f033157b0a6986639723d6d91dff6c96ce
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-17 23:17:48 -0500

    clase funciones Objetos

--- fundamentos.js
+++ fundamentos.js
@@ -1,25 +1,11 @@
-// let edad = 27;
-
-// edad += 1;
-
-// let precioVino = 23.43
-
-// let totalVino = Math.round(precioVino * 3.45)
-
-// strgTotalVino= totalVino.toFixed(2)
-
-// intTotalVino = parseFloat(strgTotalVino)
-
-// ---
-
 let emmanuel = {
     name: 'Emmanuel',
-    edad: 28,
-}
+    years: 28,
+};
 
-function nameUpperCase({name}) {
+function nameUpperCase({name}){
     console.log(name.toUpperCase());
+    
 }
-
 nameUpperCase(emmanuel)
-nameUpperCase({name: 'Lisa'})
+nameUpperCase({name:'Lisa'})
