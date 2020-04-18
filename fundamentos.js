d0c42e933279a7b5e46d85149758cb1f9f542ba9
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-18 18:55:47 -0500

    arrow function

--- fundamentos.js
+++ fundamentos.js
@@ -8,14 +8,20 @@ let ricardo={
 };
 
 const MAYORIA_EDAD = 18;
+const ES_MENOR_EDAD = (persona)=> {
+    console.log(`${persona.name} es menor de edad`);
+}
 
-function esMayorDeEdad(persona) {
-    if(persona.age >= MAYORIA_EDAD){
-        console.log(`${persona.name} es mayor de edad por que tiene ${persona.age} años`);
-    }else{
-        console.log(`${persona.name} es menor de edad por que tiene ${persona.age} años`);
+let esMayorDeEdad= (persona) => {
+    if (persona.age >= MAYORIA_EDAD) {
+    console.log(`${persona.name} es mayor de edad por que tiene ${persona.age} años`
+    );
+    } else {
+    console.log(`${persona.name} es menor de edad por que tiene ${persona.age} años`
+    );
     }
 };
 
 esMayorDeEdad(emmanuel)
-esMayorDeEdad(ricardo)
+ES_MENOR_EDAD(ricardo);
+
