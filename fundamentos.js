e89cb9bff75c0c2782a47cccfdd202e8ee2601bb
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-18 22:32:31 -0500

    ¿Cuantos días para perder peso?

--- fundamentos.js
+++ fundamentos.js
@@ -3,18 +3,26 @@ let emmanuel = {
     peso: 71
 };
 
-let random = Math.random();
+const INCREMENTO_PESO=0.3
 const DIAS_ANO = 365
-const INCREMENTO_PESO=.2 
-const PERDIDA_PESO = persona => persona.peso += INCREMENTO_PESO;
-const AUMENTO_PESO = persona => persona.peso += AUMENTO_PESO;
 
-while (condition) {
-    if (condition) {
-        
+const AUMENTO_PESO= persona => persona.peso += INCREMENTO_PESO
+const BAJAR_PESO= persona => persona.peso -= INCREMENTO_PESO
+const COME_MUCHO = () => Math.random() < 0.3
+const HACE_EJERCICIO = () => Math.random() < 0.4;
+
+const META = emmanuel.peso -3
+let dias=0;
+
+while (emmanuel.peso > META) {
+    if (COME_MUCHO()) {
+        AUMENTO_PESO(emmanuel);
     }
-    if (condition) {
-        
+    if (HACE_EJERCICIO()) {
+        BAJAR_PESO(emmanuel);
     }
+    dias += 1;
 }
-console.log(`el peso ${emmanuel.nombre} es de ${emmanuel.peso.toFixed(2)}`);
+console.log(`pasaron ${dias} hasta que ${emmanuel.name} tuvo 68kg`)
+
+// console.log(`el peso ${emmanuel.nombre} es de ${emmanuel.peso.toFixed(2)}`);
