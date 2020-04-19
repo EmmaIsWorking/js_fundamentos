8f8038693355322ee169fbe6ee4877f12173fe2c
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-19 17:12:21 -0500

    reduce

--- fundamentos.js
+++ fundamentos.js
@@ -2,21 +2,25 @@ let emmanuel={
   nombre:'Emmanuel',
   edad: 28,
   altura: 1.68,
+  libros:2,
 }
 let diana={
   nombre:'Diana',
   edad: 25,
   altura: 1.52,
+  libros: 5,
 }
 let lisa={
   nombre:'Lisa',
   edad: 38,
   altura: 1.58,
+  libros: 8,
 }
 let petter={
   nombre:'Petter',
   edad: 73,
   altura: 1.80,
+  libros: 9,
 }
 
 let personasAltas = (persona) => persona.altura > 1.70
@@ -30,10 +34,14 @@ let convertirAlturaMetro = (personas) =>({
     altura: personas.altura * 100
 })
 
+let totalLibros = (acum,personas) => acum += personas.libros
+
 let filtroPersonasAltas = personas.filter(personasAltas)
 let filtroPersonasBajas = personas.filter(personasPequeñas);
 let convertirPersonasAlturaMetro = personas.map(convertirAlturaMetro)
+let numeroDeLibros = personas.reduce(totalLibros,0)
 
 console.log(filtroPersonasBajas)
 console.log(filtroPersonasAltas)
 console.log(convertirPersonasAlturaMetro)
+console.log(numeroDeLibros);
