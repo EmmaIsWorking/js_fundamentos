53dc3157dc158a9687baf6ec7b296b2e863f8299
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-18 21:38:34 -0500

    while

--- fundamentos.js
+++ fundamentos.js
@@ -9,11 +9,12 @@ const INCREMENTO_PESO=.2
 const PERDIDA_PESO = persona => persona.peso += INCREMENTO_PESO;
 const AUMENTO_PESO = persona => persona.peso += AUMENTO_PESO;
 
-for(let i=0; i<=DIAS_ANO; i++ ){
-    if(random <.25){
-        PERDIDA_PESO(emmanuel)
-    }else if(random >.50){
-        AUMENTO_PESO(emmanuel)
+while (condition) {
+    if (condition) {
+        
+    }
+    if (condition) {
+        
     }
 }
 console.log(`el peso ${emmanuel.nombre} es de ${emmanuel.peso.toFixed(2)}`);
