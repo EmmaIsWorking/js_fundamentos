cea3274c7cb6bda74713f249170e48da34c0845f
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-19 17:39:28 -0500

    update

--- fundamentos.js
+++ fundamentos.js
@@ -7,9 +7,10 @@ function persona(nombre, apellido, anos, altura) {
 let emmanuel = new persona("Emmanuel", "Garcia", 28, 1.68);
 let ricardo = new persona("Ricardo", "Garcia", 13, 1.4);
 persona.prototype.mayorMenorEdad = function(){
-  if(this.anos < 18){
+  let mayorDeEdad = 18
+  if (this.anos < mayorDeEdad) {
     console.log(`${this.nombre} es menor de edad`);
-  }else{
+  } else {
     console.log(`${this.nombre} es mayor de edad`);
   }
 }
