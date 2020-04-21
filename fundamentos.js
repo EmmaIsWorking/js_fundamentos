859aa661433648d607f88ea91ed2c5e05c46cb28
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-20 23:46:17 -0500

    funciones como parametros

--- fundamentos.js
+++ fundamentos.js
@@ -1,11 +1,11 @@
-class Persona{
+class Persona {
   constructor(nombre, apellido, anos, altura) {
     this.nombre = nombre
     this.apellido = apellido
     this.anos = anos
     this.altura = altura
   }
-  mayorMenorEdad(){
+  mayorMenorEdad() {
     let mayorDeEdad = 18
     if (this.anos < mayorDeEdad) {
       console.log(`${this.nombre} es menor de edad`);
@@ -15,22 +15,26 @@ class Persona{
   }
 }
 
-class Desarrollador extends Persona{
+class Desarrollador extends Persona {
   constructor(nombre, apellido, anos, altura) {
     super(nombre, apellido, anos, altura)
   }
-  saludar(){console.log(`${this.nombre} es desarrollador`);}
+  saludar(fn) {
+    console.log(`Soy ${this.nombre}`);
+    if (fn === true) {
+      console.log(`${this.nombre} eres desarrollador`);
+    } else {
+      console.log(`No ${this.nombre} eres desarrollador`);
+    }
+  }
 }
-
 
 let emmanuel = new Persona("Emmanuel", "Garcia", 28, 1.68);
 let ricardo = new Persona("Ricardo", "Garcia", 13, 1.4);
+let lisa = new Desarrollador('Lisa', 'Brandau', 38, 1, 6)
+let emmanuelDev = new Desarrollador("Emmanuel", "Garcia", 28, 1.68)
 
 emmanuel.mayorMenorEdad()
 ricardo.mayorMenorEdad()
-
-let emmanuelDev = new Desarrollador("Emmanuel", "Garcia", 28, 1.68)
-
-emmanuelDev.saludar()
-
-
+emmanuelDev.saludar(true)
+lisa.saludar()
