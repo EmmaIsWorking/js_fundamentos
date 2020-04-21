27fec4633b0e442f54f5c3731f19241853cb8877
EmmaIsWorking <dimple.pleod@gmail.com>
2020-04-20 22:26:32 -0500

    creando clases y herencia (extends)

--- fundamentos.js
+++ fundamentos.js
@@ -1,21 +1,36 @@
-function persona(nombre, apellido, anos, altura) {
-  this.nombre = nombre
-  this.apellido = apellido
-  this.anos = anos
-  this.altura = altura
-}
-persona.prototype.mayorMenorEdad = function(){
-  let mayorDeEdad = 18
-  if (this.anos < mayorDeEdad) {
-    console.log(`${this.nombre} es menor de edad`);
-  } else {
-    console.log(`${this.nombre} es mayor de edad`);
+class Persona{
+  constructor(nombre, apellido, anos, altura) {
+    this.nombre = nombre
+    this.apellido = apellido
+    this.anos = anos
+    this.altura = altura
+  }
+  mayorMenorEdad(){
+    let mayorDeEdad = 18
+    if (this.anos < mayorDeEdad) {
+      console.log(`${this.nombre} es menor de edad`);
+    } else {
+      console.log(`${this.nombre} es mayor de edad`);
+    }
   }
 }
-let emmanuel = new persona("Emmanuel", "Garcia", 28, 1.68);
-let ricardo = new persona("Ricardo", "Garcia", 13, 1.4);
+
+class Desarrollador extends Persona{
+  constructor(nombre, apellido, anos, altura) {
+    super(nombre, apellido, anos, altura)
+  }
+  saludar(){console.log(`${this.nombre} es desarrollador`);}
+}
+
+
+let emmanuel = new Persona("Emmanuel", "Garcia", 28, 1.68);
+let ricardo = new Persona("Ricardo", "Garcia", 13, 1.4);
 
 emmanuel.mayorMenorEdad()
 ricardo.mayorMenorEdad()
+
+let emmanuelDev = new Desarrollador("Emmanuel", "Garcia", 28, 1.68)
+
+emmanuelDev.saludar()
 
 
